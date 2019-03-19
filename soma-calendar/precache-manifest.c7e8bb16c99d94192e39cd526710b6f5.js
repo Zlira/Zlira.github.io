47df4c2a2999f31b311a53c328dc8f68b40b4947
Zlira <olesia.hr@gmail.com>
2019-03-19 17:18:58 +0200

    Added soma calendar

---
+++ soma-calendar/precache-manifest.c7e8bb16c99d94192e39cd526710b6f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16ce0b4e6c46c65246e0",
+    "url": "/soma-calendar/static/css/main.49e6698b.chunk.css"
+  },
+  {
+    "revision": "16ce0b4e6c46c65246e0",
+    "url": "/soma-calendar/static/js/main.0441fe63.chunk.js"
+  },
+  {
+    "revision": "4df494f2c53a4379ec77",
+    "url": "/soma-calendar/static/js/runtime~main.16fc1238.js"
+  },
+  {
+    "revision": "e434210a7102a7eec144",
+    "url": "/soma-calendar/static/js/2.104a1ccf.chunk.js"
+  },
+  {
+    "revision": "246dd4f25741d671ec9495d8413984d7",
+    "url": "/soma-calendar/index.html"
+  }
+];
