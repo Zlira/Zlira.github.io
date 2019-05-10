631f1d1b0a555b76487494fa8db5cce417feff75
Zlira <olesia.hr@gmail.com>
2019-05-10 16:36:49 +0300

    new soma calendar build

---
+++ sc/precache-manifest.a2015ec2562d12bf9f9e39c0fa99e161.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a77fc6a5dd97eeca54d8",
+    "url": "./static/css/main.83d8e9f1.chunk.css"
+  },
+  {
+    "revision": "a77fc6a5dd97eeca54d8",
+    "url": "./static/js/main.0c79b14b.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "dcc697a90c05db20f180",
+    "url": "./static/js/2.28b78aa5.chunk.js"
+  },
+  {
+    "revision": "76a3fa99a5332b9885fc7e9cee42a094",
+    "url": "./index.html"
+  }
+];
