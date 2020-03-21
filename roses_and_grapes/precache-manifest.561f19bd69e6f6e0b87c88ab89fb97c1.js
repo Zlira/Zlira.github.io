05865dbdfd18f77ae66cf0c33a39b6d132e5d4cf
zlira <olesia.hr@gmail.com>
2020-03-21 21:53:24 +0200

    Better roses and better grapes

---
+++ roses_and_grapes/precache-manifest.561f19bd69e6f6e0b87c88ab89fb97c1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b76f57373151ee8575577e27b76eabb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d146e45df85783e840a",
+    "url": "./static/css/main.09345188.chunk.css"
+  },
+  {
+    "revision": "c52ee9c49f2e314eb6eb",
+    "url": "./static/js/2.f9c8028a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.f9c8028a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d146e45df85783e840a",
+    "url": "./static/js/main.93e7dcd1.chunk.js"
+  },
+  {
+    "revision": "bef2da0c5f5e0a0a5682",
+    "url": "./static/js/runtime-main.103ee1bb.js"
+  },
+  {
+    "revision": "77dafde76adb3e667fc899a1ec1d0ec0",
+    "url": "./static/media/de.77dafde7.mp3"
+  },
+  {
+    "revision": "36dc4a0c7820048b41723de8009745d8",
+    "url": "./static/media/disappointing.36dc4a0c.jpg"
+  },
+  {
+    "revision": "93c23d87cb9aeba3a9560cc95ad4db19",
+    "url": "./static/media/en.93c23d87.mp3"
+  },
+  {
+    "revision": "3218c6c11637b35b295216cdfd72efab",
+    "url": "./static/media/es.3218c6c1.mp3"
+  },
+  {
+    "revision": "6193df63fcd7da67e6c201dbea1c4197",
+    "url": "./static/media/fr.6193df63.mp3"
+  },
+  {
+    "revision": "fc169fbe6a4ab284b68e471649865c84",
+    "url": "./static/media/good.fc169fbe.jpg"
+  },
+  {
+    "revision": "7162ebee55703fa680c042c83cc9ac91",
+    "url": "./static/media/horrible.7162ebee.jpg"
+  },
+  {
+    "revision": "f873de71fa7186b17ee209e5646d702f",
+    "url": "./static/media/ja.f873de71.mp3"
+  },
+  {
+    "revision": "0c32932b3b59c10f9e2eb57d90f7ae8e",
+    "url": "./static/media/perfect.0c32932b.jpg"
+  },
+  {
+    "revision": "95824f6a3b6757bf826218f1740ac485",
+    "url": "./static/media/ru.95824f6a.mp3"
+  },
+  {
+    "revision": "c2c74f8d59741a69699c97613b704c42",
+    "url": "./static/media/sv.c2c74f8d.mp3"
+  },
+  {
+    "revision": "a641dfd1ac7ec01d693056f454375ae9",
+    "url": "./static/media/uk.a641dfd1.mp3"
+  },
+  {
+    "revision": "8200a15ba66c8dac48559f064fe8b620",
+    "url": "./static/media/zh.8200a15b.mp3"
+  }
+]);
--- roses_and_grapes/service-worker.js
+++ roses_and_grapes/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52135ba784271b61adfdfe5ee68627fa.js"
+  "./precache-manifest.561f19bd69e6f6e0b87c88ab89fb97c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
